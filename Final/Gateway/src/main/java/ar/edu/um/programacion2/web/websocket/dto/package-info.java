16eed503f29c03f4a946d51efd4c70dec9d821bf
JeronimoTaber <jerotaber@gmail.com>
2019-10-11 15:35:30 -0300

    Initial version of Gateway generated by JHipster-6.3.1

---
+++ Final/Gateway/src/main/java/ar/edu/um/programacion2/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package ar.edu.um.programacion2.web.websocket.dto;
---
+++ Final/Gateway/src/test/java/ar/edu/um/programacion2/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,9 @@
+package ar.edu.um.programacion2.cucumber.stepdefs;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
