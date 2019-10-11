1f217c8165734d5953481e0041fed644a43bbdcc
JeronimoTaber <jerotaber@gmail.com>
2019-10-11 16:10:38 -0300

    Initial version of App generated by JHipster-6.3.1

---
+++ Final/Micros/src/test/java/ar/edu/um/programacion2/app/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,9 @@
+package ar.edu.um.programacion2.app.cucumber.stepdefs;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
